d3da46527d2e4ca977b9f0a64d73e36c187954c4
gladson <gladsonbrito@gmail.com>
2020-10-10 22:47:03 -0400

    add(eslint): add config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  plugins: [
+    "import"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "max-len": "off"
+  },
+};
