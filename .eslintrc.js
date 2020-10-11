1ca0f4d4a79ca77233dd54b3a475512f11239cc8
gladson <gladsonbrito@gmail.com>
2020-10-11 05:55:20 -0400

    refactor(eslint): alterar aspas duplas para simples

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  plugins: [
-    "import"
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    "no-console": "off",
-    "no-unused-vars": "off",
-    "max-len": "off"
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ["airbnb-base"],
+    plugins: ["import"],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        "no-console": "off",
+        "no-unused-vars": "off",
+        "max-len": "off",
+    },
 };
