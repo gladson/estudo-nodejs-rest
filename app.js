3eda67dc1a960e758c1edfea627e53b46762a5d4
gladson <gladsonbrito@gmail.com>
2020-10-11 06:02:30 -0400

    refactor(app): add ponto e virgula ao final

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
 // const config = require('./config/settings')
 
-const app = express()
+const app = express();
 
 /**
  * UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'name' of undefined
- * 
+ *
  */
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // mongoose.connect(config.mongoURI, {
 mongoose.connect(process.env.MONGOURI, {
-    useNewUrlParser:true,
+    useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
-const con = mongoose.connection
+const con = mongoose.connection;
 
-con.on('open', () => {
-    console.log('MongoDB conectado...')
-})
+con.on("open", () => {
+    console.log("MongoDB conectado...");
+});
 
 // ROUTE
-app.use('/aliens', require('./routers/aliens'))
+app.use("/aliens", require("./src/routers/aliens"));
 
 // SERVER - PORT
 const port = process.env.PORT || 9000;
+const host = process.env.HOST || "http://localhost";
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta::${port}`)
-})
+    console.log(`Servidor rodando na porta => ${host}:${port}`);
+});
